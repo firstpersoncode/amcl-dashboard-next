5863c57fceb7365f4a442f72ef74dff6a76419d6
firstpersoncode <fpcdatadriver@gmail.com>
2022-08-09 23:03:47 +0800

    fix redirect

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,6 @@ export default function Index({ session, global }) {
 
 export const getServerSideProps = withSessionSsr(
   async function getServerSideProps(ctx) {
-    const isLoggedIn = ctx.req.session.getEvent("admin");
-
-    if (!isLoggedIn) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/login",
-        },
-      };
-    }
-
     return {
       props: {
         global: { page: {}, ua: ctx.req.headers["user-agent"] },
--- pages/login.jsx
+++ pages/login.jsx
@@ -14,17 +14,6 @@ export default function Login({ session, global }) {
 
 export const getServerSideProps = withSessionSsr(
   async function getServerSideProps(ctx) {
-    const isLoggedIn = ctx.req.session.getEvent("admin");
-
-    if (isLoggedIn) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/",
-        },
-      };
-    }
-
     return {
       props: {
         global: { page: {}, ua: ctx.req.headers["user-agent"] },
